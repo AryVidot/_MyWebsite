73156b348135c977b5540d1545f32dac207aab11
Ary Vidot <aryvid@gmail.com>
2020-06-09 10:58:19 +0100

    lorem ipsum change to text in lab.html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 
 // Intro Typing effect
 
+
+document.querySelector(".user-name").addEventListener("compositionstart", function (){
+    var typeName = this.
+} );
 // const texts = ["web developer", "thinker", "husband & father"]
 // let count = 0;
 // let index = 0;
+
+$("input").keypress(function(event){
+    $("#user-name").text(event.key);
+    console.log(event.key);
+})
+
+
+// Section fade in when scrolling down.
+// View Event is scroll
+
+$("section").scroll(function(event){
+    $("section").
+})
